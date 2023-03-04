6041eba2c83dcc6e6986d5687ce094f1e842c17a
nikbhargav <126935526+nikbhargav@users.noreply.github.com>
2023-03-04 12:09:23 +0530

    Add files via upload

---
+++ leap year or not.py
@@ -0,0 +1,9 @@
+num1=10
+num2=40
+num3=20
+if(num1>=num2) and (num1>=num3):
+    print("num1 is greatest")
+elif(num2>=num1) and (num2>=num3):
+    print("num2 is greatest")
+else:
+    print("num3 is greatest")
